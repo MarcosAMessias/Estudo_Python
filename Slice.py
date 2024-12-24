184539ba78c2bd05c554b34a30850f3e85349205
MarcosAMessias <115740404+MarcosAMessias@users.noreply.github.com>
2024-12-24 11:42:15 -0300

    Criação do Slice.py
    Adicionando o primeiro código

---
+++ Slice.py
@@ -0,0 +1,11 @@
+# Slice
+fruta = 'Abacate'
+# index  0123456
+print(fruta[3])
+print(fruta[2:6]) # do 2 até o último número -1
+print(fruta[-1]) #imprime a última letra
+
+valor = 99.75
+#indice 01234
+valor = str(valor) #converte para string, porque int não mostra o indice
+print(valor[3:5])
