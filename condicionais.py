4233f452e7b66d5eacd5d515b80b0d1635544455
MarcosAMessias <115740404+MarcosAMessias@users.noreply.github.com>
2024-12-25 10:25:57 -0300

    Create condicionais.py
    if, else simples

---
+++ condicionais.py
@@ -0,0 +1,12 @@
+# If, elif, else
+
+velocidade = 100
+if velocidade > 110:
+    print('Acima da velocidade permitida.')
+    print('Favor reduzir a velocidade.')
+elif velocidade < 60:
+    print('Favor dirigir acima de 80KM/h')    
+else:
+    print('Velocidade OK')
+
+print('Fim')
