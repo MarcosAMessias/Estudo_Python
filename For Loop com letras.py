5ce244d778e168eff4d681eaa4f40f3fdc0cc44f
MarcosAMessias <115740404+MarcosAMessias@users.noreply.github.com>
2024-12-29 10:47:44 -0300

    Create For Loop com letras.py

---
+++ For Loop com letras.py
@@ -0,0 +1,9 @@
+# For Loop com letras
+
+palavra = 'Google'
+for letra in palavra:
+    print('-'.join(palavra))
+
+palavra = 'Espetacular'
+for letra in palavra:
+    print(f'{letra} está dentro da palavra {palavra}')
