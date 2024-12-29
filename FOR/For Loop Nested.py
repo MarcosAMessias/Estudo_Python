3cf2397c74f6abc85b8caffd9b1c9836007fc4a0
MarcosAMessias <115740404+MarcosAMessias@users.noreply.github.com>
2024-12-29 15:22:41 -0300

    Create For Loop Nested.py
    Criando loop aninhados

---
+++ FOR/For Loop Nested.py
@@ -0,0 +1,10 @@
+"""
+for loop nested
+outer loop - loop de fora
+inner loop - loop de dentro
+"""
+for numero1 in range(5):
+    print(numero1)
+    for numero2 in range(5):
+        print(numero1, numero2)
+        
