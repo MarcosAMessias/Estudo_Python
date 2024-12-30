f8c588cd7a0869f65119ca1821db78a5113f3cb5
MarcosAMessias <115740404+MarcosAMessias@users.noreply.github.com>
2024-12-30 09:51:56 -0300

    Create For loop - criando retangulo 6x6.py

---
+++ FOR/For loop - criando retangulo 6x6.py
@@ -0,0 +1,9 @@
+# For loop - criando retangulo 6x6
+linhas = 6
+colunas = 6
+simbolo = '+'
+
+for linha in range(linhas):
+    for coluna in range(colunas):
+        print(simbolo, end='')
+    print()
