26535894a1403018a0b27c73c06afe9df32c602f
MarcosAMessias <115740404+MarcosAMessias@users.noreply.github.com>
2025-01-24 14:48:08 -0300

    Create anotações apenas.py

---
+++ FUNÇÕES/anotações apenas.py
@@ -0,0 +1,4 @@
+# Function = item no supermercado (maça - peso, cor, preço; banana - peso, cor, r$)
+# Module = converte todas funções para módulo (seção do mercado)
+# package = grupo de módulos (um supermercado)
+# library = grupo de supercados (hipermercado)
